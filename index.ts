13c3a6ab3d74fd92fc413c3417cb700f862443f3
sina-byn <sina.sina.bayan@protonmail.com>
2024-05-11 16:50:50 +0330

    initial commit - project template added

---
+++ index.ts
@@ -0,0 +1,7 @@
+import { resolveConfig } from './src/config';
+
+const init = () => {
+  const config = resolveConfig();
+};
+
+init();
---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const resolveInputs = () => {};
+
+export const resolveConfig = () => {};
