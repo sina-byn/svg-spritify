f34bdd569b343d9ec2bab07c52eccb3be100053f
sina-byn <sina.sina.bayan@protonmail.com>
2024-05-11 16:59:38 +0330

    resolveConfig read file logic added

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,22 @@
+import { sync as pkgDir } from 'pkg-dir';
+import path from 'path';
+import fs from 'fs';
+
+// * types
+type SpriteConfig = {};
+
 export const resolveInputs = () => {};
 
-export const resolveConfig = () => {};
+export const resolveConfig = () => {
+  const configPath = path.join(pkgDir() ?? process.cwd(), 'sprite.config.json');
+
+  if (!fs.existsSync(configPath)) {
+    throw new Error('failed to resolve config - config file not found');
+  }
+
+  const config: SpriteConfig = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+
+  console.log(config);
+
+  return config;
+};
