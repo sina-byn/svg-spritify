7169ba563264b4e0d10ef08d2e4d272d77a7319c
sina-byn <sina.sina.bayan@protonmail.com>
2025-07-26 14:43:07 +0330

    feat: commander added to support -i and --no-recursive CLI options

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,30 @@
 import fs from 'fs';
 import mixer from 'svg-mixer';
 
+import { program } from 'commander';
+
 // * utils
-import { generateTsType, normalizeColors } from './utils';
+import { generateTsType, hasMultipleSVGs, normalizeColors } from './utils';
 
 // * constants
+const CWD = process.cwd();
 const CLASS_NAME = 'icon';
 const FILE_NAME = 'icons.svg';
 const TS_FILE_NAME = 'icon.ts';
 
-const PATTERNS = ['**/*.svg', '!**/node_modules/**', `!${FILE_NAME}`];
+program.name('svg-spritify').description('SVG sprite generator CLI tool');
 
-const main = async () => {
+program.option('-i, --input <string>', 'entry point of the program', CWD);
+program.option('--no-recursive', "don't search for SVGs recursively", true);
+
+type Options = { input: string; recursive: boolean };
+
+program.action(async (options: Options) => {
+  const { input, recursive } = options;
+  const PATTERNS = [recursive ? '**/*.svg' : '*.svg', '!**/node_modules/**', `!${FILE_NAME}`];
+
+  process.chdir(input);
+
   const sprite = await mixer(PATTERNS, {
     prettify: true,
     spriteType: 'stack',
@@ -24,11 +37,15 @@ const main = async () => {
     },
   });
 
+  if (!hasMultipleSVGs(sprite.content)) throw new Error('Could not find any SVG files');
+
   const svg = normalizeColors(sprite.content);
   const TsType = generateTsType(svg);
 
   fs.writeFileSync(FILE_NAME, svg, 'utf-8');
   fs.writeFileSync(TS_FILE_NAME, TsType, 'utf-8');
-};
 
-main();
+  process.chdir(CWD);
+});
+
+program.parse(process.argv);
